bf88cf9ebb791d38b7e45b88fe6f81b0868ad428
GiulianoOrellana <giorellana@uade.edu.ar>
2024-11-07 09:12:20 -0300

    Add files via upload

---
+++ JavaScript/clic.js
@@ -0,0 +1,6 @@
+document.getElementById("palermo").addEventListener("click", function() {
+    window.open("https://www.google.com/maps/search/?api=1&query=Calle+Nicaragua+5900,+Palermo,+Buenos+Aires", "_blank");
+});
+document.getElementById("recoleta").addEventListener("click", function() {
+    window.open("https://www.google.com/maps/search/?api=1&query=Av.+Pueyrredon+190,+Recoleta,+Buenos+Aires", "_blank");
+});
